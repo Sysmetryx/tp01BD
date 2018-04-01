c7a5d0f9adfa05f4d22254735cea7e9cb330cbeb
Nathan <sysmetryxgosu@gmail.com>
2018-04-01 21:30:24 +0200

    Add files via upload

---
+++ tp1.py
@@ -0,0 +1,21 @@
+from google.cloud import bigquery
+
+
+def myQuerry():
+    client = bigquery.Client()
+    query_job = client.query("""
+        SELECT
+            COUNT(born_dead)
+        FROM
+            [bigquerry-public-data:samples.natality]
+        WHERE
+            (alcohol_use == TRUE and born_dead == TRUE""");
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
+
+
+if __name__ == '__main__':
+    myQuerry()
