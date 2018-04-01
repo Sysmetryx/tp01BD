3893e535317d71432adac797c575da6510efcaa8
Nathan <sysmetryxgosu@gmail.com>
2018-04-01 21:51:49 +0200

    Add files via upload

--- tp1.py
+++ tp1.py
@@ -3,13 +3,7 @@ from google.cloud import bigquery
 
 def myQuerry():
     client = bigquery.Client()
-    query_job = client.query("""
-SELECT
-            COUNT(born_dead)
-        FROM
-            'bigquerry-public-data:samples.natality'
-        WHERE
-            (alcohol_use == TRUE and born_dead == TRUE)""");
+    query_job = client.query("""SELECT COUNT(born_dead) FROM 'bigquerry-public-data:samples.natality' WHERE (alcohol_use == TRUE and born_dead == TRUE)""");
 
     results = query_job.result()  # Waits for job to complete.
 
